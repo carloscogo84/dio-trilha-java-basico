c08a88d45d461f28fb5608d0d25cf1f65650e7db
carloscogo84 <carloscogo84@gmail.com>
2024-05-08 22:56:48 +0100

    java-doc

---
+++ java-doc/src/Comentarios.java
@@ -0,0 +1,24 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        
+        // Comentário em uma única linha
+        /*
+         * Comentário 
+         * que pode ser mais detalhado
+         * quando necessario
+         */
+
+        /**
+         * Essas duas estrelas acima
+         * é para identificar que pretende
+         * elaborar um comentário
+         * a nível de documentação
+         */
+
+    
+    }
+    public void metodo(){
+        
+    }
+    
+}
