1138df85d92412b0ce90ca1face5702b90ffc4bc
carloscogo84 <carloscogo84@gmail.com>
2024-05-09 08:32:36 +0100

    meu projeto conta banco

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Executado pelo terminal");
+    }
+}
+
+/*
+ * Acessar a pasta bin
+ * ls para verificar os arquivos
+ * clear - limpar o terminal
+ * digitar o nome do arquivo sem o .class
+ */
