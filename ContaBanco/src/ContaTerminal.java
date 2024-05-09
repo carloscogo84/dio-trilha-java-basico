ed01bee4ad96d02c75ee171a7490968498d234be
carloscogo84 <carloscogo84@gmail.com>
2024-05-09 09:09:50 +0100

    update da ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Seja bem vindo ao seu banco digital");
+
+        System.out.println("Por favor, insira seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " +  numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+       
+        scanner.close();
     }
 
     
